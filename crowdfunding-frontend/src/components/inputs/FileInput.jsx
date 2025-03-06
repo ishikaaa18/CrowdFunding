5b201af6a52eea507836a698f128ce269000a96f
ishikaaa18 <ishikasaha.trp@gmail.com>
2025-03-06 22:06:25 +0530

    done

--- crowdfunding-frontend/src/components/inputs/FileInput.jsx
+++
@@ -1,9 +0,0 @@
-const FileInput = ({ label, onChange }) => (
-    <div className="form-group">
-      <label>{label}</label>
-      <input type="file" accept="image/*" onChange={onChange} />
-    </div>
-  );
-  
-  export default FileInput;
-  
--- crowdfunding-frontend/src/pages/Register.jsx
+++
@@ -1,6 +0,0 @@
-import RegisterForm from "../components/Forms/RegisterForm";
-
-const Register = () => <RegisterForm />;
-
-export default Register;
-
